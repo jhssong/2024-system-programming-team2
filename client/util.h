c85a8aef3ee49c71c5f55c208e4b51fda09cb1c4
jhssong <jhssong02@gmail.com>
2024-11-18 19:32:54 +0900

    feat: Add selecting existing team

--- client/util.h
+++ client/util.h
@@ -1,9 +1,12 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-void tty_mode(int how);
-void set_cr_noecho_mode();
-void set_non_blocking_mode();
+extern int current_line;
+
+// void tty_mode(int how);
+// void set_cr_noecho_mode();
+// void set_non_blocking_mode();
 void show_title(const char* title);
+int select_item(const char* item_list[], int list_size);
 
 #endif
