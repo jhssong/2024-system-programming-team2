f4135ebafe8e0ef4808f7f68667f646d21ea05a5
jhssong <jhssong02@gmail.com>
2024-11-19 10:38:43 +0900

    chore: Remove useless codes

--- client/util.c
+++ client/util.c
@@ -6,42 +6,7 @@
 #include <curses.h>
 #include <string.h>
 
-// #define NUMBER_OF_CHAR_AT_A_TIME 1
 #define TITLE_START_LINE 3
-
-// void tty_mode(int how) {
-// 	static struct termios orig_mode;
-
-// 	if (how == 0) 					// Save current setting
-// 		tcgetattr(0, &orig_mode); 
-// 	else if (how == 1)				// Recover saved setting
-// 		tcsetattr(0, TCSANOW, &orig_mode);
-// }
-
-// void set_cr_noecho_mode() {
-// 	struct termios ttyinfo;
-	
-// 	if (tcgetattr(0, &ttyinfo) == -1) {
-// 		perror("tcgetattr");
-// 		exit(EXIT_FAILURE);
-// 	}
-
-// 	ttyinfo.c_lflag &= ~ICANON;		// Set non-canonical mode
-// 	ttyinfo.c_lflag &= ~ECHO;		// Disable ECHO bit
-// 	ttyinfo.c_cc[VMIN] = NUMBER_OF_CHAR_AT_A_TIME;
-
-// 	if (tcsetattr(0, TCSANOW, &ttyinfo) == -1) {
-// 		perror("tcsetattr");
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
-
-// void set_non_blocking_mode() {
-//     int terflags;
-//     terflags = fcntl(0, F_GETFL);
-//     terflags |= O_NONBLOCK;
-//     fcntl(0, F_SETFL, terflags);
-// }
 
 void show_title(const char* title) {
 	move(TITLE_START_LINE, 0);
@@ -125,6 +90,5 @@ int select_item(const char* item_list[], int list_size) {
 				break;
 		}
 	}
-
 	return -1;
 }
--- client/util.h
+++ client/util.h
@@ -3,9 +3,6 @@
 
 extern int current_line;
 
-// void tty_mode(int how);
-// void set_cr_noecho_mode();
-// void set_non_blocking_mode();
 void show_title(const char* title);
 int select_item(const char* item_list[], int list_size);
 
