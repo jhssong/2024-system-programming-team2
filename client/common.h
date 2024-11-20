32915dfce714f3a99e420ed2723446e3f7d1517d
jhssong <jhssong02@gmail.com>
2024-11-20 16:57:00 +0900

    chore: Remove common folders

--- client/common.h
+++ client/common.h
@@ -26,4 +26,31 @@
 #define MENU_START_ROW 4
 #define MENU_START_COL 36
 
+typedef struct {
+	char team_list[8][256];
+} Team_list;
+
+typedef struct {
+	int team_id;
+	char team_pw[9];
+	char team_name[256];
+	int created_time;
+} Team_detail;
+
+typedef struct {
+	char username[256];
+	short schedule[26][7];
+} Personal_Table;
+
+typedef struct{
+	short team_table[26][7];
+} Team_table;
+
+typedef union {
+	Team_list team_list;
+	Team_detail team_detail;
+	Personal_Table personal_table;
+	Team_table team_table;
+} Server_response;
+
 #endif
