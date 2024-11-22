07166950ec7514904c220bf45fb2cd104a8b2371
jhssong <jhssong02@gmail.com>
2024-11-23 02:06:13 +0900

    feat: Add new data sturct

---
+++ client/common.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+user_info user_info;
+team_info team_info;
+team_list team_list;
--- client/common.h
+++ client/common.h
@@ -18,42 +18,72 @@
 #define APP_VERSION "v0.0.1"
 
 #define TERMINAL_MIN_WIDTH 50
-#define TERMINAL_MIN_HEIGHT 30 			// 09:00 ~ 22:00
+#define TERMINAL_MIN_HEIGHT 30	// 09:00 ~ 22:00
 
-#define TEAM_TABLE_START_ROW 4
-#define TEAM_TABLE_START_COL 2
-#define PERSONAL_TABLE_START_ROW 4
-#define PERSONAL_TABLE_START_COL 19
-#define MENU_START_ROW 4
-#define MENU_START_COL 36
+#define TEAM_TABLE_START_ROW 4		// TODO remove
+#define TEAM_TABLE_START_COL 2		// TODO remove
+#define PERSONAL_TABLE_START_ROW 4	// TODO remove
+#define PERSONAL_TABLE_START_COL 19	// TODO remove
+#define MENU_START_ROW 4			// TODO remove
+#define MENU_START_COL 36			// TODO remove
 
-#define TEAM_FOLDER_MODE 0777
-#define TEAM_CONFIG_FILE_MODE 0644
-
-#define MAX_TEAM_FOLDER_PATH 100
-#define MAX_TEAM_CONFIG_FILE_PATH 120
-
-#define MAX_TEAM_COUNT 8
-#define MAX_TEAM_ID_LEN 9
-#define MAX_TEAM_NAME_LEN 51
-#define MAX_TEAM_PW_LEN 9
-#define MAX_USER_NAME_LEN 51
+#define MAX_NAME_SIZE 51
+#define MAX_PW_SIZE 9
 #define CREATED_TIME_LEN 20
 
+#define MAX_TEAM_COUNT 8
+#define MAX_TEAM_ID_LEN 9  // TODO remove
+
 typedef struct {
-	char team_list[MAX_TEAM_COUNT][MAX_TEAM_NAME_LEN];
+	char team_name[MAX_NAME_SIZE];
+	char user_name[MAX_NAME_SIZE];
+	char user_pw[MAX_PW_SIZE];
+} user_info;
+
+typedef struct {
+	char team_name[MAX_NAME_SIZE];
+	char team_pw[MAX_PW_SIZE];
+	char created_time[CREATED_TIME_LEN];
+} team_info;
+
+typedef struct {
+	char team_list[MAX_TEAM_COUNT][MAX_NAME_SIZE];
+	int size;
+} team_list;
+
+typedef struct {
+	char user_name[MAX_NAME_SIZE];
+	char team_name[MAX_NAME_SIZE];
+	short user_table[TABLE_MAX_TIME][TABLE_MAX_DAY];
+} update_user_table;
+
+typedef union {
+	user_info user_info;
+	team_list team_list;
+	team_info team_info;
+	update_user_table update_user_table;
+} request;
+
+extern user_info user_info;
+extern team_info team_info;
+extern team_list team_list;
+
+// deprecated ---------------------------------------------- 
+
+typedef struct {
+	char team_list[MAX_TEAM_COUNT][MAX_NAME_SIZE];
 	int size;
 } Team_list;
 
 typedef struct {
 	char team_id[MAX_TEAM_ID_LEN];
-	char team_pw[MAX_TEAM_PW_LEN];
-	char team_name[MAX_TEAM_NAME_LEN];
+	char team_pw[MAX_PW_SIZE];
+	char team_name[MAX_NAME_SIZE];
 	char created_time[CREATED_TIME_LEN];
 } Team_detail;
 
 typedef struct {
-	char username[MAX_USER_NAME_LEN];
+	char username[MAX_NAME_SIZE];
 	short schedule[26][7];
 } Personal_Table;
 
