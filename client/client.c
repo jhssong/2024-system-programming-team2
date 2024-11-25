8fcd5132aa7c3b18c554dd53a0c144a28183def8
tbl0415 <tbl0415@knu.ac.kr>
2024-11-25 18:35:21 +0900

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,100 +1,117 @@
 #include "client.h"
 
-#define PORT 8080
-#define BUFFER_SIZE 100
+response connect_to_server(request_packet req) {
+    int sock;
+    int received_bytes;
+    struct sockaddr_in serv_addr;
 
-void print_team_table(short team_table[TABLE_MAX_TIME][TABLE_MAX_DAY]) {
-    printf("      Sun   Mon   Tue   Wed   Thu   Fri   Sat\n");
+    response_packet res;
 
-    for (int i = 0; i < TABLE_MAX_TIME; i++) {
-        int hour = 9 + (i / 2); 
-        char half = (i % 2 == 0) ? 'A' : 'B'; 
-        printf("%02d%c    ", hour, half); 
-
-        
-        for (int j = 0; j < TABLE_MAX_DAY; j++) {
-            int count = team_table[i][j];
-            
-            // Color and content based on count
-            if (count == 0) {
-                printf(" X   "); // No availability
-            } else if (count == 1) {
-                printf("\033[38;5;159m■     \033[0m"); //Lightest blue
-            } else if (count == 2) {
-                printf("\033[38;5;153m■     \033[0m"); 
-            } else if (count == 3) {
-                printf("\033[38;5;117m■     \033[0m"); 
-            } else if (count == 4) {
-                printf("\033[38;5;111m■     \033[0m"); 
-            } else if (count == 5) {
-                printf("\033[38;5;75m■     \033[0m"); 
-            } else if (count == 6) {
-                printf("\033[38;5;69m■     \033[0m"); 
-            } else if (count == 7) {
-                printf("\033[38;5;33m■     \033[0m"); 
-            } else if (count == 8) {
-                printf("\033[38;5;18m■     \033[0m"); // Darkest blue
-            }
-        }
-        printf("\n");
-    }
-}
-
-
-Server_response function_0(int sock) {
-    // get team_directory via server response
-    Server_response response;
-    memset(&response, 0, sizeof(Server_response));
-
-    int bytes_received = 0;
-
-    bytes_received = recv(sock, &response, sizeof(Server_response), 0);
-    if(bytes_received<0){
-        perror("Fail to receive team_directory\n");
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1){
+        perror("socket() error");
+        exit(EXIT_FAILURE);
     }
 
-    return response;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    serv_addr.sin_port = htons(PORT);
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+        perror("Connection failed");
+        close(sock);
+        // exit(EXIT_FAILURE);
+        return res.res;
+    }
+    
+    received_bytes = recv(sock, &res, sizeof(response_packet), 0);
+
+    if (received_bytes <= 0) {
+        mvprintw(window_height - 1, 0, "Failed to receive data\n");
+        refresh();
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (res.status_code == 503) {
+        mvprintw(window_height - 1, 0, "[ERROR] 503 error: %s\n", res.msg);
+        refresh();
+        close(sock);
+        exit(EXIT_FAILURE);
+    } else {
+    #ifdef DEBUG
+        printw("[DEBUG] status %d with: %s\n", res.status_code, res.msg);
+        refresh();
+    #endif
+    }
+
+    send(sock, &req, sizeof(request_packet), 0);
+
+    received_bytes = recv(sock, &res, sizeof(response_packet), 0);
+
+    if (received_bytes <= 0) {
+        mvprintw(window_height - 1, 0, "Failed to receive data\n");
+        refresh();
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (res.status_code == 503) {
+        mvprintw(window_height - 1, 0, "%s", res.msg);
+        refresh();
+        close(sock);
+    } else {
+    #ifdef DEBUG
+        printw("[DEBUG] status %d with: %s\n", res.status_code, res.msg);
+        refresh();
+    #endif
+    }
+    return res.res;
 }
 
-// connect to 127.0.0.1:8080
-void connect_to_server()
-{
+void old_connect_to_server(int function_choose, Server_response req_data) {
     int sock;
     struct sockaddr_in serv_addr;
+
+    int received_bytes;
     char msg[BUFFER_SIZE];
     
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1){
-        perror("socket() error");
+        mvprintw(window_height - 1, 0, "socket() error");
+        // perror("socket() error");
         exit(1);
     }
     
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
     serv_addr.sin_port = htons(PORT);
     
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
-        perror("Connection failed");
-        close(sock);
-        exit(1);
-    }
-
-    int check = recv(sock, msg, BUFFER_SIZE, 0);
-    msg[check] = '\0';
-    if(strcmp(msg, "Server full") == 0){
-        perror("Server is full!\nWait until other client disconnect server!");
+        mvprintw(window_height - 1, 0, "Connection failed");
+        // perror("Connection failed");
         close(sock);
         exit(EXIT_FAILURE);
-    }else{
-        printf("Connected to Server\n");
+    }
+
+    if((received_bytes = recv(sock, msg, BUFFER_SIZE, 0)) <= 0){
+        mvprintw(window_height - 1, 0, "Cannot get server status");
+		// perror("Cannot get server status");
+        close(sock);
+        exit(EXIT_FAILURE);
+	}
+
+    msg[received_bytes] = '\0';
+    if(strcmp(msg, "Server full") == 0){
+        mvprintw(window_height - 1, 0, "Server is full!");
+        refresh();
+        close(sock);
+        exit(EXIT_FAILURE);
     }
     
-
-    int func = 0;
-    printf("Enter what function to do: ");
-    scanf("%d", &func);
-    send(sock, &func, sizeof(func), 0);
+    send(sock, &function_choose, sizeof(function_choose), 0);
 
     int function_received;
     Server_response response;
@@ -104,38 +121,69 @@ void connect_to_server()
     switch(function_received)
     {
         case 0: // Will Receive Team_list via server_response union
-            response = function_0(sock);
-            // will place show team_list function
+        memset(&response, 0, sizeof(Server_response));
+
+        Team_list team_list;
+        if ((received_bytes = recv(sock, &team_list, sizeof(team_list), 0)) <= 0) {
+            perror("Failed to receive team list");
+            close(sock);
+            exit(EXIT_FAILURE);
+        }
+        int check = 0;
+        printf("%d\n", team_list.size);
+        printf("%s %s\n", team_list.team_list[0], team_list.team_list[1]);
+        while(check!=team_list.size){
+            printf("While문 안임");
+            printf("%d: %s\n", check, team_list.team_list[check]);
+            check ++;
+        }
+            
         break;
 
-        case 1: // will receive Team_detail via server_response union
+        case 1: // will receive Team_detail via server_response union            
+            send(sock, &req_data.team_detail, sizeof(Team_detail), 0);
+	        memset(&response, 0, sizeof(Server_response));
 
-        break;
+            Team_detail new_team;
+            if ((received_bytes = recv(sock, &new_team, sizeof(Team_detail), 0)) <= 0) {
+                perror("Failed to receive team data");
+                getchar();
+                break;
+            }
+            break;
 
         case 2: // will receive Personal_table via server_response union
+            send(sock, &req_data.personal_table, sizeof(Personal_Table), 0);
+            memset(&response, 0, sizeof(Server_response));
 
-        break;
+            Personal_Table loaded_user_table;
+            if((received_bytes = recv(sock, &loaded_user_table, sizeof(Personal_Table), 0)) <= 0){
+                perror("Failed to receive team data");
+                getchar();
+                break;
+            }
+            // load_user_table(loaded_user_table);  // FIXME
+            break;
 
         case 3: // will receive Team_table via server_response union
-            req_packet.cmd = 3; // Command for requesting team table
-            send(sock, &req_packet, sizeof(request_packet), 0);
-
-        
-            if ((received_bytes = recv(sock, &res_packet, sizeof(response_packet), 0)) <= 0) {
-                perror("Failed to receive team table data");
+            strncpy(req_packet.req.user_table.team_name, team_info.team_name, MAX_NAME_SIZE);
+            
+            if (send(sock, &req_packet, sizeof(request_packet), 0) <= 0) {
+                perror("Failed to send team table request");
                 close(sock);
                 exit(EXIT_FAILURE);
             }
 
-            if (res_packet.status_code == 200) {
-                printf("Received Team Table:\n");
-                printf_team_table(response_packet.res.team_table);
-            } else {
-                printf("Failed to get team table: %s\n", res_packet.msg);
+            if ((received_bytes = recv(sock, &res_packet, sizeof(response_packet), 0)) <= 0) {
+                perror("Failed to receive team table response");
+                close(sock);
+                exit(EXIT_FAILURE);
             }
-            break;
+            
+            if (res_packet.status_code == 200) {
+                memcpy(team_table, res_packet.res.team_table, sizeof(team_table));
             } else {
-                printf("Failed to get team table: %s\n", res_packet.msg);
+                printf("Failed to get team table);
             }
             break;
     }
