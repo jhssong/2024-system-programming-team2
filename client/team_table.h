677115f5fbc2c2d827850f1b0e0bfe0b98866268
p1utie <plutie@naver.com>
2024-11-27 05:15:18 +0900

    Add new team table codes

---
+++ client/team_table.h
@@ -0,0 +1,11 @@
+#ifndef TEAM_TABLE_H
+#define TEAM_TABLE_H
+
+#include "common.h"
+
+#define TEAM_TABLE_START_ROW 4
+#define TEAM_TABLE_START_COL 2
+
+void print_team_table(short team_table[TABLE_MAX_TIME][TABLE_MAX_DAY]);
+
+#endif
